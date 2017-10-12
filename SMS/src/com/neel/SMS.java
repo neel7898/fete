7d3e197cd1d2f3d499a5fbda53e5018169b974ee
neel7898 <neelabhsharma.95@gmail.com>
2017-10-12 22:35:09 +0530

    SMS application

---
+++ SMS/src/com/neel/SMS.java
@@ -0,0 +1,94 @@
+package com.neel;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class SMS {
+	
+	private final String USER_AGENT = "Mozilla/5.0";
+	
+	public static void main(String[] args) throws Exception {
+		SMS sms = new SMS();
+		sms.sendPost();
+		sms.sendGet();
+	}
+	
+	private void sendGet() throws Exception {
+
+		String url = "http://smsgateway.me/api/v3/devices?email=lifeeditor.94@gmail.com&password=7898555253";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+	// HTTP POST request
+	private void sendPost() throws Exception {
+
+		String url = "http://smsgateway.me/api/v3/messages/send";
+		URL obj = new URL(null, url, new sun.net.www.protocol.https.Handler());
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add request header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		String urlParameters = "email=lifeeditor.94@gmail.com&password=7898555253&device=63397&number=7898555253&message=HELLO";
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+}
